f441b6d256c3ac355b6c5898213c21343c07bb18
PiotrMazgaj <mazgaj.piotr@wp.pl>
2016-03-22 12:13:20 +0100

    the end

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
+function maxArray() {
+
+}
 (function () {
     'use strict';
     window.app = {
@@ -22,8 +25,39 @@
             if (a < 0 || b < 0 || h < 0 || typeof a !== 'number' || typeof b !== 'number' || typeof h !== 'number') {
                 return false;
             }
+            maxArray();
             return 0.5 * h * (a + b);
-        }
+        },
+        maxArray: function(t){
+            var max;
+            max=0;
+            for(var i=0; i< t.length; i++) {
+                if (typeof t[i] !== 'number') {
+                    return false;
+                }
+                else {
+                    if (t[i] > max) {
+                        max = t[i];
+                    }
+                    if (i == t.length - 1) {
+                        return max;
+                    }
+                }
+            }
+        },
+        squareOdd: function (t) {
+            for(var i=0; i< t.length; i++) {
+                if (typeof t[i] !== 'number') {
+                    return false;
+                }
+                else {
+                    if (t[i]%2!=0) {
+                        t[i] = t[i]*t[i];
+                    }
+                }
+            }
+            return t ;
+    }
     }
 })();
 
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,7 +2,7 @@ describe('app', function () {
     'use strict';
     var app = window.app;
 
-    xdescribe('Division function', function () {
+    describe('Division function', function () {
         it('should divide number a by b', function () {
             expect(app.divide(10, 5)).toEqual(2);
             expect(app.divide(15, 3)).toEqual(5)
@@ -46,7 +46,7 @@ describe('app', function () {
         xit('counting down type', function () {
             expect(app.getDescendingNumbers(10,5)).toEqual(typeof descendingNumbers === 'number' );
         });
-});
+    });
 
     describe('areaOfTrapezoid function', function () {
         it('area with a = -10', function () {
@@ -57,6 +57,31 @@ describe('app', function () {
         });
         it('area with h = -2', function () {
             expect(app.areaOfTrapezoid(10,5,-2)).toEqual(false);
+        });
+    });
+
+    describe('maxArray function', function () {
+        it('area with others things', function () {
+            expect(app.maxArray([1,1,2])).toEqual(2);
+        });
+        it('empty', function () {
+            expect(app.maxArray(5)).not.toBe(null);
+        });
+        it('other type', function () {
+            expect(app.maxArray(["assd",5,2])).toEqual(false);
+        });
+    });
+
+    describe('squareOdd function', function () {
+        it('area with others things', function () {
+            expect(app.squareOdd([1,1,2])).toEqual([1,1,2]);
+            expect(app.squareOdd([1,3,2])).toEqual([1,9,2]);
+        });
+        it('empty', function () {
+            expect(app.squareOdd(5)).not.toBe(null);
+        });
+        it('other type', function () {
+            expect(app.squareOdd(["assd",5,2])).toEqual(false);
         });
     });
 });
